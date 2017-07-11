86839db6909d83f9e09a60e77c83e4fd1a5570de
kayllan <kayllanm@gmail.com>
2017-07-11 13:06:13 +0200

    first project commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brightskillsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the brightskillsApp
+ */
+angular.module('brightskillsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brightskillsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the brightskillsApp
+ */
+angular.module('brightskillsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
